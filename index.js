aa83dc3fa76df211ae2e487be6707969d37c2b78
DLJ1984 <devon.leigh.johnson@protonmail.com>
2023-02-16 11:07:22 +0800

    add css blank, add html button, add js prompt player selection while loop

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-console.log("Hello World!");
+function playerPrompt() {
+    let selection = false;
+
+    while (selection == false) {
+    
+        let input = prompt("rock, paper or scissors?");
+        let choice = input.toLowerCase();
+
+        if (choice == "rock" || choice =="paper" || choice == "scissors") {
+        
+            selection = true;
+            let playerSelection = choice;
+            return playerSelection;
+        
+        } else {
+        
+            window.alert("Please type in a selection of either Rock, Paper or Scissors");
+
+        }
+
+    }
+
+} 
+
+function getComputerChoice () {
+    const choices = ["rock", "paper", "scissors"];
+    const choice = Math.floor(Math.random() * choices.length);
+    return choice;
+}
+
+function rockPaperScissors(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        
+        let result = "Draw!";
+        return result;
+
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        
+        let result = "You Lose! paper beats rock!";
+        return result;
+
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        
+        let result = "You Lose! rock beats scissors!";
+        return result;
+
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        
+        let result = "You Lose! scissors beats paper!";
+        return result;
+        
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        
+        let result = "You Win! paper beats rock!";
+        return result;
+
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        
+        let result = "You Win! rock beats scissors!";
+        return result;
+
+    } else {
+        
+        let result = "You Win! scissors beats paper!";
+        return result;
+    }
+
+}
+
+
